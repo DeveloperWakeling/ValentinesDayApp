a3d98d784717b739eee5535e5a24f885a8eb1ab4
Michael W <michael@developerwakeling.com>
2019-02-14 17:29:55 -0500

    Initial Commit

---
+++ ValentinesDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ValentinesDay
+//
+//  Created by Michael Wakeling on 2/14/19.
+//  Copyright © 2019 Michael Wakeling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
